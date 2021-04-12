a014a626bc57c9698e7132883518779018f5c3a2
Matthias Kestenholz <mk@feinheit.ch>
2021-04-12 15:41:57 +0200

    Add a hook for showing validation warnings

--- feincms3_forms/admin.py
+++ feincms3_forms/admin.py
@@ -1,10 +1,46 @@
+from urllib.parse import quote as urlquote
+
 from content_editor.admin import ContentEditor, ContentEditorInline
+from django.contrib import messages
+from django.contrib.admin.utils import quote
 from django.core.exceptions import ImproperlyConfigured
+from django.urls import reverse
+from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 
 
+NO_CONTINUE_PARAMETERS = {"_addanother", "_save", "_saveasnew"}
+
+
 class ConfiguredFormAdmin(ContentEditor):
-    pass
+    # Possible hook for validation, with stack hacking: _create_formsets
+
+    def validate_configured_form(self, request, obj):
+        opts = obj._meta
+        obj_url = reverse(
+            "admin:%s_%s_change" % (opts.app_label, opts.model_name),
+            args=(quote(obj.pk),),
+            current_app=self.admin_site.name,
+        )
+        if self.has_change_permission(request, obj):
+            obj_repr = format_html('<a href="{}">{}</a>', urlquote(obj_url), obj)
+        else:
+            obj_repr = str(obj)
+        messages.info(
+            request, format_html(_('"{obj}" has been validated.'), obj=obj_repr)
+        )
+
+    def save_related(self, request, form, formsets, change):
+        super().save_related(request, form, formsets, change=change)
+        if request.method == "POST" and any(
+            key in request.POST for key in NO_CONTINUE_PARAMETERS
+        ):
+            self.validate_configured_form(request, form.instance)
+
+    def render_change_form(self, request, context, *, obj, **kwargs):
+        if obj and request.method == "GET":
+            self.validate_configured_form(request, obj)
+        return super().render_change_form(request, context, obj=obj, **kwargs)
 
 
 class SimpleFieldInline(ContentEditorInline):
