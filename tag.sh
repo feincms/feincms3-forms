c8529c1dea1cddd564432e17ca7a3d5764b60af7
Matthias Kestenholz <mk@feinheit.ch>
2021-04-16 15:03:31 +0200

    Do not stop when dist doesn't exist

--- tag.sh
+++ tag.sh
@@ -6,6 +6,6 @@ git commit -m "feincms3-forms $TAG"
 git tag -m "feincms3-forms $TAG" "$TAG"
 git push --all
 git push --tags
-rm -r dist
+rm -rf dist
 python3 setup.py sdist bdist_wheel
 twine upload dist/*
