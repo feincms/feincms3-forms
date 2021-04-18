2a758a4e4c34d1118d5e20f42a13c47ce601b029
Matthias Kestenholz <mk@feinheit.ch>
2021-04-18 14:43:40 +0200

    Require Python 3.9 so that we can use | to merge dicts

--- feincms3_forms/renderer.py
+++ feincms3_forms/renderer.py
@@ -1,5 +1,6 @@
 from functools import reduce
 from hashlib import sha1
+from operator import or_
 
 from django import forms
 
@@ -18,22 +19,19 @@ def create_form(items, *, form_class=forms.Form, form_kwargs):
     item_fields = {
         item: item.get_fields() for item in items if hasattr(item, "get_fields")
     }
-    all_fields = reduce(lambda a, b: {**a, **b}, item_fields.values(), {})
+    all_fields = reduce(or_, item_fields.values(), {})
     all_names = set(all_fields)
 
     initial = form_kwargs.get("initial", {})
     for item in items:
         if hasattr(item, "get_initial") and (item_initial := item.get_initial()):
-            initial = {**item_initial, **initial}
+            initial = item_initial | initial
     form_kwargs["initial"] = initial
 
     form = type("Form", (FormMixin, form_class), all_fields)(**form_kwargs)
     form._f3f_item_fields = {
-        **{
-            item: {name: form[name] for name in fields}
-            for item, fields in item_fields.items()
-        },
-        **{None: {name: form[name] for name in form.fields if name not in all_names}},
-    }
+        item: {name: form[name] for name in fields}
+        for item, fields in item_fields.items()
+    } | {None: {name: form[name] for name in form.fields if name not in all_names}}
 
     return form
