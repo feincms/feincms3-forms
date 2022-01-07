38f0f8a897ff36f99be989e133902e4926de451b
Matthias Kestenholz <mk@feinheit.ch>
2022-01-07 21:05:55 +0100

    pre-commit

--- tests/manage.py
+++ tests/manage.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import os
 import sys
-from os.path import abspath, dirname
+from os.path import dirname
 
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testapp.settings")
 
-    sys.path.insert(0, dirname(dirname(abspath(__file__))))
+    sys.path.insert(0, dirname(dirname(__file__)))
 
     from django.core.management import execute_from_command_line
 
